ed9d0a606917b859e1504e38f423e5bdbe1868d7
Stormgren <sbnv93@gmail.com>
2023-08-18 22:36:24 +0200

    created Blogs controller

--- part4/backend/index.js
+++ part4/backend/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
+const blogsRouter = require('./controllers/blogs')
 /*
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -21,7 +21,7 @@ mongoose.connect(config.MONGO_URI);
 
 app.use(cors())
 app.use(express.json())
-
+/*
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
@@ -39,8 +39,11 @@ app.post('/api/blogs', (request, response) => {
       response.status(201).json(result)
     })
 })
+*/
 
+app.use('/api/blogs',blogsRouter)
 // const PORT = process.env.PORT || 3003
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
+
