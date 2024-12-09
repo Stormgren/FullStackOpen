3ddd095db2389bc01fba41314cccff05704edcdb
Stormgren <sbnv93@gmail.com>
2024-12-09 22:49:02 +0100

    Refactored exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,8 +1,26 @@
-import Togglable from "./Togglable.jsx";
+
+import {useEffect, useState} from "react";
 
 
 const Blog = ({ blog }) => {
-  const blogStyle = {
+    const [isVisible, setIsVisible] = useState(false);
+    const [btnLabel, setBtnLabel] = useState('show')
+
+    const showWhenVisible = {display: isVisible ? '' : 'none'}
+    
+    useEffect(() => {
+        if(isVisible){
+            setBtnLabel('hide')
+        } else {
+            setBtnLabel('show')
+        }
+    }, [isVisible]);
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible)
+
+    }
+
+    const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
     border: 'solid',
@@ -12,11 +30,13 @@ const Blog = ({ blog }) => {
   return (
       <div style={blogStyle}>
 
-        {blog.title} <Togglable buttonLabel='Show'>
+          {blog.title}  <button onClick={toggleVisibility}>{btnLabel}</button>
+      <div style={showWhenVisible}>
           Author: {blog.author}<br/>
           Url: {blog.url}<br/>
           Likes: {blog.likes}<br/>
-        </Togglable>
+
+      </div>
       </div>
   )
 }
