151c02deba6565063d2d2d7a35e439f44aa530f6
Stormgren <sbnv93@gmail.com>
2024-12-09 21:57:14 +0100

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,24 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from "./Togglable.jsx";
+
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  return (
+      <div style={blogStyle}>
+
+        {blog.title} <Togglable buttonLabel='Show'>
+          Author: {blog.author}<br/>
+          Url: {blog.url}<br/>
+          Likes: {blog.likes}<br/>
+        </Togglable>
+      </div>
+  )
+}
 
 export default Blog
